f8ce00efd0286961d0624c133aca7fe3c683fb45
saw-code <saw.coding@gmail.com>
2021-11-26 14:21:33 +0300

    fix method from interface(more ruby way)

--- lib/console_interface.rb
+++ lib/console_interface.rb
@@ -26,9 +26,7 @@ class ConsoleInterface
   end
 
   def word_to_show
-    @game.letters_to_guess.map do |letter|
-      letter || '__'
-    end.join(" ")
+    @game.letters_to_guess.map { |letter| letter || '__' }.join(' ')
   end
 
   def errors_to_show
